ae9eca7b2b9f2be251723c64213ca27799b6a915
rafi00000 <delwar2021bd@gmail.com>
2024-01-25 14:57:25 +0600

    added mongodb uri

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/hrVertex")
+        await mongoose.connect("mongodb+srv://hrVertexserver:hrvertexserver7712@cluster0.mn7153h.mongodb.net/?retryWrites=true&w=majority")
         console.log("db is connected")
     }
     catch (error) {
