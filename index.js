3044cf2296626cae72239237e5fd697b8d155f71
rafi00000 <delwar2021bd@gmail.com>
2024-02-01 16:59:52 +0600

    add: application api

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require('dotenv').config()
-const cors = require("cors");
+require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-const userRoute = require("./routes/user");
+const userRoute = require('./routes/user');
 const recruitmentRoutes = require('./routes/recruitmentRoute');
-const { default: mongoose } = require("mongoose");
-
+const { default: mongoose } = require('mongoose');
+const applicationRouter = require('./routes/application');
 
 // middlewares
-app.use(cors({
-    origin: ["https://hr-vertex-client.vercel.app", "http://localhost:3000"],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: ['https://hr-vertex-client.vercel.app', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CONNECTION
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@hrvertex.rr1tlxn.mongodb.net/?retryWrites=true&w=majority`)
-    .then(data => {
-        console.log("db connected")
-    })
-    .catch(err => {
-        console.log("error while connecting");
-    })
-
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@hrvertex.rr1tlxn.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then((data) => {
+    console.log('db connected');
+  })
+  .catch((err) => {
+    console.log(process.env.DB_PASSWORD)
+    console.log('error while connecting');
+  });
 
 // routes
-app.use("/users", userRoute);
+app.use('/users', userRoute);
 app.use('/recruitment', recruitmentRoutes);
+app.use("/applications", applicationRouter);
 
 // for run test
-app.get("/", (req, res) => res.send("Server is running"));
+app.get('/', (req, res) => res.send('Server is running'));
 app.listen(port, async () => {
-    console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
--- models/application.js
+++ models/application.js
@@ -1,11 +1,37 @@
-const { default: mongoose } = require("mongoose");
-
-
+const { default: mongoose } = require('mongoose');
 
 const applicationSchema = mongoose.Schema({
-    
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    default: 'Null',
+  },
+  education: {
+    type: String,
+    required: true,
+  },
+  experience: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  resume: {
+    type: String,
+    required: true,
+  }
+});
 
-
-const applicationModel = mongoose.model("application", applicationSchema);
-export default applicationModel;
+const applicationModel = mongoose.model('application', applicationSchema);
+module.exports = applicationModel;
